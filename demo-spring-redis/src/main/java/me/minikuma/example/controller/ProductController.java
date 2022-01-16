c91e6bf05cb8f1b019246799ebd68e3f735da637
minikuma <minikuma@naver.com>
2022-01-16 23:00:10 +0900

    Product POST API

--- demo-spring-redis/src/main/java/me/minikuma/example/controller/ProductController.java
+++ demo-spring-redis/src/main/java/me/minikuma/example/controller/ProductController.java
@@ -1,7 +1,62 @@
 package me.minikuma.example.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import me.minikuma.example.common.dto.BaseResponse;
+import me.minikuma.example.common.dto.ProductRequestDto;
+import me.minikuma.example.entity.Product;
+import me.minikuma.example.service.ProductService;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @RequestMapping(method = RequestMethod.OPTIONS)
+    public ResponseEntity<?> options() {
+        //@formatter:off
+        return ResponseEntity
+                .ok()
+                .allow(HttpMethod.GET, HttpMethod.POST, HttpMethod.HEAD)
+                .build();
+        //@formatter:on
+    }
+
+    // TODO: 상품 저장 (단건) Controller
+    @PostMapping("/save")
+    public ResponseEntity<?> insertProduct(@RequestBody ProductRequestDto.Save request) {
+        Product savedProduct = productService.saveProduct(request);
+
+        URI uri = MvcUriComponentsBuilder.fromController(this.getClass())
+                .path("/{productId}")
+                .buildAndExpand(savedProduct.getProductId())
+                .toUri();
+
+        // TODO: savedProduct -> BaseResponse Object 변환
+        BaseResponse response = BaseResponse.builder()
+                .apiVersion("V1")
+                .statusCode(HttpStatus.CREATED.value())
+                .resultCode("0000")
+                .resultMessage("정상 등록되었습니다.")
+                .payload(savedProduct)
+                .build();
+
+        return ResponseEntity.created(uri).body(response);
+    }
+
+    // TODO: 상품 저장 (다건)
+
+    // TODO: 상품 조건 (단건, ID 기준)
+
+    // TODO: 상품 조건 (다건, 멀티 Query 조건, name, price)
 }
--- demo-spring-redis/src/main/java/me/minikuma/example/service/ProductService.java
+++ demo-spring-redis/src/main/java/me/minikuma/example/service/ProductService.java
@@ -2,8 +2,11 @@ package me.minikuma.example.service;
 
 import lombok.RequiredArgsConstructor;
 import me.minikuma.example.common.dto.BaseResponse;
+import me.minikuma.example.common.dto.ProductRequestDto;
 import me.minikuma.example.entity.Product;
 import me.minikuma.example.repository.ProductRepository;
+import org.springframework.boot.web.server.Http2;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,8 +18,17 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     // TODO: 상품 저장 (단건)
-    public void saveProduct() {
+    public Product saveProduct(final ProductRequestDto.Save request) {
 
+        // TODO: Save Request -> Product Object 로 변경
+        Product product = Product.builder()
+                .productName(request.getProductName())
+                .productDescription(request.getProductDescription())
+                .price(request.getPrice())
+                .quantity(request.getQuantity())
+                .build();
+
+        return productRepository.save(product);
     }
 
     // TODO: 상품 저장 (다건)
