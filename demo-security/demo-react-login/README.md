873e7ce3b7211d31c10bbc7f320e1a4b20b14b23
minikuma <minikuma@naver.com>
2022-04-07 23:19:01 +0900

    [FEAT] react.js login page

---
+++ demo-security/demo-react-login/README.md
@@ -0,0 +1,4 @@
+### Demo React Login Page
+
+---
+Spring Security 테스트를 위한 React.js 로 만든 간단한 로그인 페이지 입니다.
---
+++ demo-security/demo-react-login/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Link
+} from 'react-router-dom';
+import Signin from './Signin';
+import Profile from './Profile';
+
+function App() {
+  const token = localStorage.getItem('accessToken');
+
+  if(!token) {
+    return <Signin />
+  }
+
+  return (
+      <div className="wrapper">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/profile"><Profile /></Route>
+            <Route path="/"><Profile /></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+  );
+}
+
+export default App;
