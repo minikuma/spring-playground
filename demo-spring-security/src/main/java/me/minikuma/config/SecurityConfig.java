bb1b77ffd919dc53a74526fad63b4385aac3672a
minikuma <minikuma@naver.com>
2022-07-11 23:20:22 +0900

    [FEAT] form login authentication filter

--- demo-spring-security/src/main/java/me/minikuma/config/SecurityConfig.java
+++ demo-spring-security/src/main/java/me/minikuma/config/SecurityConfig.java
@@ -5,7 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * 사용자 정의 보안 기능
@@ -15,10 +24,38 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin();
+
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+        ;
+
+        http
+                .formLogin()
+                //.loginPage("/loginPage") // 사용자 정의 로그인 페이지
+                .defaultSuccessUrl("/") // 로그인 성공 후 이동 페이지
+                .failureUrl("/login")
+                .usernameParameter("userId") // 아이디 파라미터 설정
+                .passwordParameter("pwd") // 패스워드 파라미터 설정
+                .loginProcessingUrl("/login_process") // 로그인 form action url
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        System.out.println(">>>> authentication :: " + authentication.getName());
+                        response.sendRedirect("/");
+                    }
+                }) // 로그인 성공 후 핸들러
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                        System.out.println(">>>> exception :: " + exception.getMessage());
+                        response.sendRedirect("/login");
+                    }
+                }) // 로그인 실패 후 핸들러
+                .permitAll()
+        ;
+
         return http.build();
     }
 }
