f702b8b0e0b71574a1d25ee227efc77b0174ecb9
minikuma <minikuma@naver.com>
2022-07-12 23:20:33 +0900

    [FEAT] security filter
    * Remember me filter
    * logout filter

---
+++ demo-spring-security/README.md
@@ -0,0 +1,16 @@
+## Spring Security
+
+---
+
+### Features
+
+(1) Security API & Filter
+* Remember Me Filter
+  * ```RememberMeAuthenticationFilter```
+    * Remember Me Cookie 를 발급받은 경우
+    * 인증(Authentication) 객체가 없는 경우
+
+
+
+
+---
--- demo-spring-security/src/main/java/me/minikuma/config/SecurityConfig.java
+++ demo-spring-security/src/main/java/me/minikuma/config/SecurityConfig.java
@@ -7,13 +7,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -22,6 +26,13 @@ import java.io.IOException;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
@@ -54,6 +65,37 @@ public class SecurityConfig {
                     }
                 }) // 로그인 실패 후 핸들러
                 .permitAll()
+        ;
+
+        // logout
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .deleteCookies("JESSIONID", "remember-me")
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                })
+                .deleteCookies("remember-me")
+        ;
+
+        // remember me 활성화
+        http
+                .rememberMe()
+                .rememberMeParameter("remember") // 기본 파라미터 명은 remember-me
+                .tokenValiditySeconds(3600) // Default 14일
+                .alwaysRemember(false) // remember me 기능이 활성화되지 않아도 항상 실행
+                .userDetailsService(userDetailsService)
         ;
 
         return http.build();
